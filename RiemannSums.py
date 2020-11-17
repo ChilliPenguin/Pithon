f50f8d8a4f26239dd3c4bfb547f854f43186379e
ChilliPenguin <70456316+ChilliPenguin@users.noreply.github.com>
2020-11-16 22:55:51 -0500

    Added cool graphs

--- RiemannSums.py
+++ RiemannSums.py
@@ -1,14 +1,21 @@
 from tkinter import *
 import math as m
 from sympy import *
+from PIL import ImageTk,Image 
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from matplotlib.patches import Rectangle
 
 #Graph Variables
 GraphEquation = ''
 GraphInterval = ''
 x = Symbol('x')
+mainGraph = Figure(figsize=(5,5), dpi=100)
+
 
 #Arithmetic stuff
 interval = [1,3]
@@ -28,6 +35,7 @@ def RectCaluclator(giv):
 
 def findVerticalAsymptotes(): #finds the vertical asymptotes that are limits to the interval
     global equation
+    
     onDisvior = False
     parathesesCounter = 0
     interval = [0,0]
@@ -74,32 +82,53 @@ def callback(P):
 
 def Riemmanstuff():
     global equation
-    try:
-        equation = str(equationFunctionInput.get())
-        numberOfRect = int(amountOfRectangles.get())
-        interval[0]   = int(leftIntervalInput.get())
-        interval[1]   = int(rightIntervalInput.get())
-        GraphInterval = [interval[0]-0.05*abs(interval[0]),interval[0]+0.05*abs(interval[0])]
-        GraphEquation = equation
-        intervalLimits = findVerticalAsymptotes()
-        if numberOfRect > m.pow(10,6):
-            resultShower['text'] ='You have inputed a super large number, which will crash the program!'
-            return
-        estimatedArea = 0
-        print(intervalLimits)
-        for i in intervalLimits:
-            if i >= interval[0] and i <= interval[1]:
-                resultShower['text'] ='The interval is at a vertical asymptote! The program has detected x = ' + str(intervalLimits)
-                return
-        totalBase = abs(interval[1]-interval[0])
-        for t in range(0, numberOfRect) :
-            leftcurrent = (t)* totalBase/numberOfRect+interval[0]
-            area = totalBase/numberOfRect * RectCaluclator(leftcurrent)
-            estimatedArea += area
-        resultShower['text'] = 'Estimated: '+ str(estimatedArea)
+    #try:
+    equation = str(equationFunctionInput.get())
+    numberOfRect = int(amountOfRectangles.get())
+    interval[0]   = int(leftIntervalInput.get())
+    interval[1]   = int(rightIntervalInput.get())
+    GraphInterval = [interval[0]-0.05*abs(interval[0]),interval[1]+0.05*abs(interval[1])]
+    GraphEquation = equation
 
-    except:
-        resultShower['text'] ='An error occured, check if values are correctly inputed'
+    mainGraph.clf()
+    GraphSubPlot = mainGraph.add_subplot(111)
+    GraphSubPlot.set(title='Le Graph')
+    GraphSubPlot.grid()
+
+
+    x = np.array(np.arange(GraphInterval[0],GraphInterval[1],0.01))
+    y = eval(GraphEquation)
+    GraphSubPlot.plot(x,y)
+
+    canvas.draw()
+    currentAxis = mainGraph.gca()
+
+    
+
+
+    intervalLimits = findVerticalAsymptotes()
+    if numberOfRect > m.pow(10,6):
+        resultShower['text'] ='You have inputed a super large number, which will crash the program!'
+        return
+    estimatedArea = 0
+    print(intervalLimits)
+    for i in intervalLimits:
+        if i >= interval[0] and i <= interval[1]:
+            resultShower['text'] ='The interval is at a vertical asymptote! The program has detected x = ' + str(intervalLimits)
+            return
+    totalBase = abs(interval[1]-interval[0])
+    for t in range(0, numberOfRect) :
+        leftcurrent = (t)* totalBase/numberOfRect+interval[0]
+        yValue = RectCaluclator(leftcurrent)
+        currentAxis.add_patch(Rectangle((leftcurrent, 0), totalBase/numberOfRect,yValue, facecolor="grey"))
+        area = totalBase/numberOfRect * yValue
+        
+        estimatedArea += area
+    canvas.draw()
+    resultShower['text'] = 'Estimated: '+ str(estimatedArea)
+
+#except:
+    resultShower['text'] ='An error occured, check if values are correctly inputed'
 
 
 #GUI STUFF
@@ -107,6 +136,7 @@ def Riemmanstuff():
 
 #main stuff
 root = Tk()
+canvas = FigureCanvasTkAgg(mainGraph, master=root)
 vcmd = (root.register(callback)) #gets inputs
 root.geometry("1200x800")
 root.title("Riemann sum calculator")
@@ -159,5 +189,6 @@ amountOfRectangles.pack(anchor='n')
 
 calculateButton.pack(anchor='n')
 resultShower.pack(anchor='s')
+canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
 
 root.mainloop() # runs main loop
