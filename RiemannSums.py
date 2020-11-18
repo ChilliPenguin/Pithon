c13f0e26084cd87d8c08cd346a6f2b91e86605ff
ChilliPenguin <70456316+ChilliPenguin@users.noreply.github.com>
2020-11-17 21:49:31 -0500

    Commented code, add tool tip
    Next stuff prob will be decimal inputs and better gui

--- RiemannSums.py
+++ RiemannSums.py
@@ -6,7 +6,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 from matplotlib.patches import Rectangle
 
@@ -23,123 +23,126 @@ nonallowed =[]
 typeOfSum = 0
 numberOfRect = 0
 equation =""
+#an enumerator
 class optionsOfSums(enum.Enum):
     Left = 0
     Right = 1
 
-
+#Calculates y-value of function
 def RectCaluclator(giv):
-    global equation
-    res = parse_expr(equation).subs(x,giv)
-    return res
+    global equation                         #makes sure equation is actual equation
+    res = parse_expr(equation).subs(x,giv)  #caluclates it
+    return res                              #returns value
 
 def findVerticalAsymptotes(): #finds the vertical asymptotes that are limits to the interval
     global equation
     
-    onDisvior = False
+    #Values to make find divisors(vertical asymptotes occur when divisors = 0)
+    onDisvior = False   
     parathesesCounter = 0
     interval = [0,0]
     problems = []
     nonallowed = []
     counter = 0
     intervalcounter = 0
-    for i in equation+" ":
+    for i in equation+" ":  #loops through each char of equation
         
-        if onDisvior:
+        if onDisvior:   #runs when inside a divisor
             if i == "(":
                 parathesesCounter+=1
-                if equation[intervalcounter-1] not in ["/","*","+","-"]:
-                    equation = ''.join((equation[:intervalcounter],"*",equation[intervalcounter:]))
-                    intervalcounter+=1
-                    counter+=1
-            elif i == ")":
+                if equation[intervalcounter-1] not in ["/","*","+","-"]:                            #Checks if something like 3a turns to 3*a
+                    equation = ''.join((equation[:intervalcounter],"*",equation[intervalcounter:])) #adds a multiplication sign to make sure equation is readable
+                    intervalcounter+=1  #makes sure offset is correct
+                    counter+=1          #^^
+            elif i == ")":  #goes down one nested parathesis
                 parathesesCounter-=1
             elif parathesesCounter == 0:
                 if i in ["*","/","x"] or i.isdigit():
-                    pass
+                    pass    #makes sure to not end on multiplication, division, or x
                 else:
                     onDisvior = False
                     interval[1] = counter
                     problems.append(equation[interval[0]+1:interval[1]])
                     inverval = [0,0]
-        elif i == "/":
-            onDisvior = True
-            interval[0] = counter
-        counter += 1
-        intervalcounter +=1
-    x = Symbol('x')
-    for p in problems:
-        for s in solve(p):
-            nonallowed.append(s)
-    return nonallowed
-def callback(P):
+        elif i == "/":              #If a divisor is detected, cant nest due to elif
+            onDisvior = True        #if a / is found, then it is a divisor(tells program)
+            interval[0] = counter   #sets  the start of divisor
+        counter += 1                #adds to the offset to make sure code is referencing the correct character
+        intervalcounter +=1         #increases the interval counter by 1
+    x = Symbol('x')                 # makes sure that x is the symbol
+    for p in problems:              #loops through every divisor(their equations)
+        for s in solve(p):          #Finds all solutions in the given problem
+            nonallowed.append(s)    # appends all solutions to the not allowed array(to prevent infinite areas)
+    return nonallowed               #returns all non allowed domains
+def callback(P):        #limits inputs of certain text inputs to numbers(neg and pos)
     if str.isdigit(P[1:]) or P[1:] =="" :
         if P == "" or str.isdigit(P[0]) or P[0] in ["-"]:
             return True
         return False
     else:
-        return False
+        return False    
 
-def Riemmanstuff():
-    global equation
-    #try:
-    equation = str(equationFunctionInput.get())
-    numberOfRect = int(amountOfRectangles.get())
-    interval[0]   = int(leftIntervalInput.get())
-    interval[1]   = int(rightIntervalInput.get())
-    GraphInterval = [interval[0]-0.05*abs(interval[0]),interval[1]+0.05*abs(interval[1])]
-    GraphEquation = equation
+def Riemmanstuff(): #Main calculations
+    global equation #makes sure equation is the equation
+    try:            #attempts to calculate, will fail(try here to prevent code from dying)
+        equation        = str(equationFunctionInput.get())  #obtains equation from equation input box
+        numberOfRect    = int(amountOfRectangles.get())     #obtains the amount of rectangles to draw
+        interval[0]     = int(leftIntervalInput.get())      #obtains first part of interval
+        interval[1]     = int(rightIntervalInput.get())     #obtains second part of interval
+        GraphInterval   = [interval[0],interval[1]]         #creates the graph interval, also not needed
+        GraphEquation   = equation                          #not really needed, but its cool
 
-    mainGraph.clf()
-    GraphSubPlot = mainGraph.add_subplot(111)
-    GraphSubPlot.set(title='Le Graph')
-    GraphSubPlot.grid()
+        #Graph graphical stuff
+        mainGraph.clf()                                 #clears current graph from prevent instance
+        GraphSubPlot = mainGraph.add_subplot(111)       #adds subplot to main graph figure
+        GraphSubPlot.grid()                             #adds grid to graph
 
+        #sets equation of the graph
+        x = np.array(np.arange(GraphInterval[0],GraphInterval[1],0.01))
+        y = eval(GraphEquation)
+        GraphSubPlot.plot(x,y)  #plots it
 
-    x = np.array(np.arange(GraphInterval[0],GraphInterval[1],0.01))
-    y = eval(GraphEquation)
-    GraphSubPlot.plot(x,y)
-
-    canvas.draw()
-    currentAxis = mainGraph.gca()
-
-    
-
-
-    intervalLimits = findVerticalAsymptotes()
-    if numberOfRect > m.pow(10,6):
-        resultShower['text'] ='You have inputed a super large number, which will crash the program!'
-        return
-    estimatedArea = 0
-    print(intervalLimits)
-    for i in intervalLimits:
-        if i >= interval[0] and i <= interval[1]:
-            resultShower['text'] ='The interval is at a vertical asymptote! The program has detected x = ' + str(intervalLimits)
-            return
-    totalBase = abs(interval[1]-interval[0])
-    for t in range(0, numberOfRect) :
-        leftcurrent = (t)* totalBase/numberOfRect+interval[0]
-        yValue = RectCaluclator(leftcurrent)
-        currentAxis.add_patch(Rectangle((leftcurrent, 0), totalBase/numberOfRect,yValue, facecolor="grey"))
-        area = totalBase/numberOfRect * yValue
         
-        estimatedArea += area
-    canvas.draw()
-    resultShower['text'] = 'Estimated: '+ str(estimatedArea)
+        #updates graphical stuff
+        toolbar.update()
+        canvas.draw()
+        currentAxis = mainGraph.gca()
 
-#except:
-    resultShower['text'] ='An error occured, check if values are correctly inputed'
+        intervalLimits = findVerticalAsymptotes()   #limits domain to prevent nonpossible calculations
+        if numberOfRect > m.pow(10,6):              #prevents computer death
+            resultShower['text'] ='You have inputed a super large number, which will crash the program!'
+            return                                  #gets out of function
+        estimatedArea = 0           #intilize estimateed area from rectangles
+        for i in intervalLimits:    #makes sure domain doesnt cross over limits
+            if i >= interval[0] and i <= interval[1]:
+                resultShower['text'] ='The interval is at a vertical asymptote! The program has detected x = ' + str(intervalLimits)
+                return
+        totalBase = abs(interval[1]-interval[0])    #calculates total base of the graph
+        for t in range(0, numberOfRect) :           #calculates every rectangle
+            leftcurrent = (t)* totalBase/numberOfRect+interval[0]
+            yValue = RectCaluclator(leftcurrent)    #calculates y cordinate                 !!!!!!!!!!!!!!ADD ON HERE ABOUT PIVOT TYPE!!!!!!!!
+            currentAxis.add_patch(Rectangle((leftcurrent, 0), totalBase/numberOfRect,yValue, facecolor="grey")) #adds rectangle to graph, HARDWARE INTENSIVE, can cause lag
+            area = totalBase/numberOfRect * yValue  #calculates area of rectangle
+            estimatedArea += area                   #adds to estimated area
+        canvas.draw()                               #draws all rectangles, is also  HARDWARE INTENSIVE 
+        resultShower['text'] = 'Estimated: '+ str(estimatedArea)    #shows total estimated area
+    except: #when something fails, prevents total death(ie no input)
+        resultShower['text'] ='An error occured, check if values are correctly inputed'
 
 
 #GUI STUFF
 #Button stuff
 
-#main stuff
+#root
 root = Tk()
+#canvas
 canvas = FigureCanvasTkAgg(mainGraph, master=root)
-vcmd = (root.register(callback)) #gets inputs
+toolbar = NavigationToolbar2Tk(canvas, root)
+
+vcmd = (root.register(callback)) #gets inputs for certain text inputs(to limit)
 root.geometry("1200x800")
 root.title("Riemann sum calculator")
+
 #Widgets
 SubTitle = Label(root, text="Riemann sum calculator", font=("Courier", 10))
 typeTitle = Label(root, text="Type/Starting point")
